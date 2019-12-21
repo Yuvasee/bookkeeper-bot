2b1bb986040aeee5d0db3afaaa0692e805a4e94d
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2019-12-21 21:16:36 +0200

    fixed rates, added dump

---
+++ src/commands/makeDump.ts
@@ -0,0 +1,41 @@
+import { Message } from 'node-telegram-bot-api';
+import TelegramBot = require('node-telegram-bot-api');
+import XLSX = require('xlsx');
+
+import Command from '../interfaces/Command';
+import Transaction, { TransactionDoc } from '../models/Transaction';
+import RateToIls, { RateToIlsDoc } from '../models/Rate';
+import { error } from '../views/error';
+
+const makeDump: Command = {
+    trigger: /^dump$/,
+
+    reaction: (bot: TelegramBot) => async (msg: Message, match: RegExpExecArray) => {
+        try {
+            const [transactionDocs, rateDocs] = await Promise.all([Transaction.find(), RateToIls.find()]);
+            const transactions = transactionDocs.map(a => a.toObject() as TransactionDoc);
+            const rates = rateDocs.map(a => a.toObject() as TransactionDoc);
+
+            const book = XLSX.utils.book_new();
+            book.Sheets.transactions = XLSX.utils.json_to_sheet(transactions);
+            book.Sheets.rates = XLSX.utils.json_to_sheet(rates);
+            book.SheetNames.push('transactions', 'rates');
+
+            bot.sendDocument(
+                msg.chat.id,
+                XLSX.write(book, {
+                    type: 'buffer',
+                }),
+                {},
+                {
+                    filename: 'dump.xlsx',
+                    contentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                },
+            );
+        } catch (err) {
+            bot.sendMessage(msg.chat.id, error(err));
+        }
+    },
+};
+
+export default makeDump;
