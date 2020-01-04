924f0ffdfb79f9e82f2d077d730e924d664b3515
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2020-01-04 20:00:54 +0200

    tr ls month parameter

--- src/commands/tr/ls.ts
+++ src/commands/tr/ls.ts
@@ -1,4 +1,4 @@
-import { startOfMonth, endOfMonth } from 'date-fns';
+import { startOfMonth, endOfMonth, getMonth, getYear, subMonths } from 'date-fns';
 import { ParseMode } from 'node-telegram-bot-api';
 
 import Command from '../../interfaces/Command';
@@ -10,11 +10,20 @@ const trLs: Command = {
     name: 'tr ls',
 
     reaction: bot => async (msg, match) => {
+        const { text } = msg;
+
+        const innerMatch = text.match(/^tr\s+ls\s+(-?\d{1,2})(?:\s+(\d{4}))?\s*/);
+        const monthMatch = Number(innerMatch[1]);
+        const yearMatch = Number(innerMatch[2]);
+
+        const [month, year] = getMonthYearFromMatch(monthMatch, yearMatch);
+        const [start, end] = getStartEnd(month, year);
+
         try {
             const transactions = await Transaction.find()
                 .where('date')
-                .gte(startOfMonth(new Date()))
-                .lte(endOfMonth(new Date()));
+                .gte(start)
+                .lte(end);
 
             const message = transactionListView(transactions);
 
@@ -26,5 +35,33 @@ const trLs: Command = {
         }
     },
 };
+
+function getMonthYearFromMatch(monthMatch: number, yearMatch: number): [number, number] {
+    const now = new Date();
+    // Default month is currect
+    let baseDate = now;
+    let month = 0;
+    let year = 0;
+
+    // -n from current month
+    if (monthMatch <= 0) {
+        baseDate = subMonths(now, -monthMatch);
+    } else if (monthMatch <= 12 && yearMatch) {
+        // exact month
+        month = monthMatch - 1;
+        // exact year or current year by default
+        year = yearMatch || (monthMatch <= getMonth(now) ? getYear(now) : getYear(now) - 1);
+    }
+
+    month = month || getMonth(baseDate);
+    year = year || getYear(baseDate);
+
+    return [month, year];
+}
+
+function getStartEnd(month: number, year: number): [Date, Date] {
+    const baseDate = new Date(year, month);
+    return [startOfMonth(baseDate), endOfMonth(baseDate)];
+}
 
 export default trLs;
