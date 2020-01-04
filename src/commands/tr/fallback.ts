6038b9bdd86e7e29f7487b754577a69c3cd17a7e
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2020-01-04 22:10:10 +0200

    tr doc update

--- src/commands/tr/fallback.ts
+++ src/commands/tr/fallback.ts
@@ -2,12 +2,25 @@ import Command from '../../interfaces/Command';
 import { error } from '../../views/error';
 
 const text = `
-usage: tr <command>
+new transaction:
+-nn[(.|,)nn][:<cat>][ #<tag>]*[ $<currency][ @mm/dd/yyyy][ <description>]
+-12,3:food
+-12,3:food #greens #auchan $usd @10/19/2019 just some food for evening
 
-commands:
-list [mm[ yyyy]] - view transactions list for current or specified month
-update <id> <new minus command> - update transaction
-delete <id> - delete transaction
+list month transactions:
+tr ls [[-]mm[ yyyy]]
+tr ls 11
+tr ls -1
+tr ls 10 2018
+
+alter transaction:
+tr alt <id> [-nn[(.|,)nn]][ :<cat>][ #<tag>]*[ $<currency][ @mm/dd/yyyy][ <description>]
+tr alt 5e10c7b1b8d9734e67b048ab :food $usd
+tr alt 5e10c7b1b8d9734e67b048ab -12,3:food #greens just some food for evening
+
+remove transaction
+tr rm <id>
+tr rm 5e10c7b1b8d9734e67b048ab
 `;
 
 const trFallback: Command = {
