9e7e79c7e8b3a4d6ac0dc01c2accf552d7a08c43
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2020-01-04 22:22:23 +0200

    tr rm command

--- src/commands/tr/index.ts
+++ src/commands/tr/index.ts
@@ -3,10 +3,11 @@ import CommandSet from '../../interfaces/CommandSet';
 import fallback from './fallback';
 import ls from './ls';
 import alt from './alt';
+import rm from './rm';
 
 export const trSet: CommandSet = {
     name: 'tr',
-    commands: { ls, alt },
+    commands: { ls, alt, rm },
     fallback,
 };
 
---
+++ src/commands/tr/rm.ts
@@ -0,0 +1,29 @@
+import { ParseMode } from 'node-telegram-bot-api';
+
+import Command from '../../interfaces/Command';
+import Transaction from '../../models/Transaction';
+import { error } from '../../views/error';
+
+const trRm: Command = {
+    name: 'tr rm',
+
+    reaction: bot => async (msg, match) => {
+        const { text } = msg;
+
+        const innerMatch = text.match(/^tr\s+rm\s+([a-fA-F\d]{24})\s*/);
+        const [, id] = innerMatch;
+
+        const chatId = msg.chat.id;
+        const options = { parse_mode: 'HTML' as ParseMode };
+        Transaction.findByIdAndDelete(id, (err, res) => {
+            if (err) {
+                bot.sendMessage(chatId, error(err), options);
+            } else {
+                !res && bot.sendMessage(chatId, `Transaction ${id} not found. Nothing done.`);
+                res && bot.sendMessage(chatId, `Transaction ${res.id} deleted (-${res.sum} ${res.currency}).`);
+            }
+        });
+    },
+};
+
+export default trRm;
